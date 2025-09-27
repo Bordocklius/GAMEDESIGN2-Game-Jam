14c7cf5a8f8d0227b5052e49dae1386a184786aa
Bordocklius <miguel.dhaenens@hotmail.com>
2025-09-27 18:57:39 +0200

    Started big arena

---
+++ Assets/Scripts/TargetSpawner.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _spawnPoints;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
